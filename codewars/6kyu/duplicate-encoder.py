db1582ddf81bcacb8a0088e980ed519350fb9e6a
Sam Frances <sam@samfrances.uk>
2018-05-19 23:15:12 +0100

    Solution to https://www.codewars.com/kata/54b42f9314d9229fd6000d9c

---
+++ codewars/6kyu/duplicate-encoder.py
@@ -0,0 +1,33 @@
+#!#!/usr/bin/env python
+
+"""
+The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string, or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples:
+
+"din" => "((("
+
+"recede" => "()()()"
+
+"Success" => ")())())"
+
+"(( @" => "))(("
+"""
+
+from collections import defaultdict
+
+
+def count_letters(word):
+    counts = defaultdict(int)
+    for letter in word:
+        counts[letter] += 1
+    return counts
+
+
+def duplicate_encode(word):
+    lower = word.lower()
+    counts = count_letters(lower)
+    return ''.join(
+        "(" if counts[letter] == 1 else ")"
+        for letter in lower
+    )
