3178b4bb28028e7c1dd5876154d11a023586cb8c
Sam Frances <sam@samfrances.uk>
2018-05-30 10:09:35 +0100

    Javascript solution to https://www.codewars.com/kata/title-case

---
+++ codewars/6kyu/title_case.js
@@ -0,0 +1,57 @@
+/*
+Solution to https://www.codewars.com/kata/title-case/
+
+A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the
+first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case
+unless it is the first word, which is always capitalised.
+
+Write a function that will convert a string into title case, given an optional list of exceptions (minor words).
+The list of minor words will be given as a string with each word separated by a space. Your function should ignore
+the case of the minor words string -- it should behave in the same way even if the case of the minor word string is
+changed.
+
+###Arguments (Haskell)
+
+First argument: space-delimited list of minor words that must always be lowercase except for the first word in the
+  string.
+Second argument: the original string to be converted.
+###Arguments (Other languages)
+
+First argument (required): the original string to be converted.
+Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word
+  in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+
+###Example
+
+titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
+titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
+titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
+ */
+
+function titleCase(sentence, minor_words = "") {
+
+  const minor_trimed = minor_words.trim();
+  const sentence_trimed = sentence.trim();
+
+  if (sentence_trimed.length === 0) {
+    return "";
+  }
+
+  const minor =
+    minor_trimed.length === 0
+      ? []
+      : minor_trimed.toLowerCase().split(" ");
+
+  const words = sentence_trimed.toLowerCase().split(" ");
+
+  const first = titleCaseSingleWord(words[0]);
+  const rest =
+    words
+    .slice(1)
+    .map(word => minor.includes(word) ? word : titleCaseSingleWord(word));
+
+  return [first, ...rest].join(" ");
+}
+
+const titleCaseSingleWord = word =>
+  word[0].toUpperCase() + word.slice(1).toLowerCase();
