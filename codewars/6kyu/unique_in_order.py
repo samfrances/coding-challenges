83081c0c386edaf31a02ca766f955b518ef20da7
Sam Frances <sam@samfrances.uk>
2018-05-31 10:04:51 +0100

    Add link to kata URL in latest solution

--- codewars/6kyu/unique_in_order.py
+++ codewars/6kyu/unique_in_order.py
@@ -1,6 +1,8 @@
 #!#!/usr/bin/env python2
 
 """
+Solution to https://www.codewars.com/kata/54e6533c92449cc251001667/
+
 Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 
 For example:
