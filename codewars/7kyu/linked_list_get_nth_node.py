11d4086562ba79b7f20ef6780febef9eb1bcc722
Sam Frances <sam@samfrances.uk>
2018-07-08 17:02:05 +0100

    Starting point for /kata/linked-lists-get-nth-node/

---
+++ codewars/7kyu/linked_list_get_nth_node.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Solution to http://www.codewars.com/kata/linked-lists-get-nth-node/
+"""
+
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def get_nth(node, index):
+    pass
+    # Your code goes here.
