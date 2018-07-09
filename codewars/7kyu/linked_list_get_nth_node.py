6dce04a539452795c05093f3cade27494a1db86c
Sam Frances <sam@samfrances.uk>
2018-07-09 20:36:29 +0100

    Solution to /kata/55befc42bfe4d13ab1000007/

--- codewars/7kyu/linked_list_get_nth_node.py
+++ codewars/7kyu/linked_list_get_nth_node.py
@@ -2,6 +2,22 @@
 
 """
 Solution to http://www.codewars.com/kata/linked-lists-get-nth-node/
+
+Linked Lists - Get Nth
+
+Implement a GetNth() function that takes a linked list and an integer index and
+returns the node stored at the Nth index position. GetNth() uses the C
+numbering convention that the first node is index 0, the second is index 1,
+... and so on. So for the list 42 -> 13 -> 666, GetNth() with index 1 should
+return Node(13);
+
+getNth(1 -> 2 -> 3 -> null, 0).data === 1
+getNth(1 -> 2 -> 3 -> null, 1).data === 2
+
+The index should be in the range [0..length-1]. If it is not, GetNth() should
+throw/raise an exception (ArgumentException in C#, InvalidArgumentException in
+PHP). You should also raise an exception (ArgumentException in C#,
+InvalidArgumentException in PHP) if the list is empty/null/None.
 """
 
 
@@ -12,5 +28,8 @@ class Node(object):
 
 
 def get_nth(node, index):
-    pass
-    # Your code goes here.
+    if node is None:
+        raise IndexError("Index out of range")
+    if index == 0:
+        return node
+    return get_nth(node.next, index - 1)
